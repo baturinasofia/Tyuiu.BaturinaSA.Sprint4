1bbff2b0a96ee9cb197d7baced78fec4ad19cd66
baturinasofia <sofi1608@bk.ru>
2025-11-09 22:21:31 +0500

    Task4

---
+++ Tyuiu.BaturinaSA.Sprint4.Task4.V13.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.BaturinaSA.Sprint4.Task4.V13.Lib
+{
+    public class DataService : ISprint4Task4V13
+    {
+        public int Calculate(int[,] matrix)
+        {
+            int sum = 0;
+            foreach (int c in matrix)
+            {
+                if (c % 2 == 0) sum += c;
+            }
+            return sum;
+        }
+    }
+}
---
+++ Tyuiu.BaturinaSA.Sprint4.Task4.V13.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.BaturinaSA.Sprint4.Task4.V13.Lib;
+
+namespace Tyuiu.BaturinaSA.Sprint4.Task4.V13.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int[,] numsArray = new int[5, 5] 
+            { 
+              { 6, 3, 5, 7, 5 }, 
+              { 3, 5, 7, 4, 5 }, 
+              { 5, 6, 4, 5, 4 }, 
+              { 7, 4, 7, 3, 3 }, 
+              { 4, 6, 5, 5, 6 } 
+            };
+            int res = ds.Calculate(numsArray);
+            Assert.AreEqual(44, res);
+        }
+    }
+}
