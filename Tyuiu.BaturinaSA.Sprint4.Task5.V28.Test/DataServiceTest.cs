e1f73b3a21dcaaf46606f292a0b5083a82025a98
baturinasofia <sofi1608@bk.ru>
2025-11-10 11:18:29 +0500

    Task5

---
+++ Tyuiu.BaturinaSA.Sprint4.Task5.V28.Test/DataServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.BaturinaSA.Sprint4.Task5.V28.Lib;
+
+namespace Tyuiu.BaturinaSA.Sprint4.Task5.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[,] array = new int[,] 
+            {
+                { -6, -2, 1, 3, -2 },
+                { -3, 2, -1, 1, -2 }, 
+                { 1, 3, 1, 2, -4 },
+                { -3, 2, 0, -5, 1 }, 
+                { -1, 2, -4, 1, -6 } 
+            };
+            int[,] result = new int[,] 
+            { 
+                { 0, 0, 1, 3, 0 }, 
+                { 0, 2, 0, 1, 0 }, 
+                { 1, 3, 1, 2, 0 }, 
+                { 0, 2, 0, 0, 1 }, 
+                { 0, 2, 0, 1, 0 } 
+            };
+            CollectionAssert.AreEqual(result, ds.Calculate(array));
+        }
+    }
+}
---
+++ Tyuiu.BaturinaSA.Sprint4.Task5.V28/Program.cs
@@ -0,0 +1,59 @@
+﻿using Tyuiu.BaturinaSA.Sprint4.Task5.V28.Lib;
+
+namespace Tyuiu.BaturinaSA.Sprint4.Task5.V28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Random rnd = new Random();
+            Console.Title = "Спринт #4 │ Выполнила: Батурина С.А. │ ИСТНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы (случайная генерация)                           *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #28                                                             *");
+            Console.WriteLine("* Выполнила: Батурина Софья Алексеевна │ ИСТНб-25-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+            Console.WriteLine("* случайными значениями в диапазоне от -6 до 3. Заменить отрицательные    *");
+            Console.WriteLine("* элементы на 0.                                                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+
+            int[,] matrix = new int[5, 5];
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    matrix[i, j] = rnd.Next(-6, 3);
+                }
+            }
+            Console.WriteLine("\nМассив");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write("{0}\t", matrix[i, j]);
+                }
+            }
+            Console.WriteLine("\n***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            var result = ds.Calculate(matrix);
+            Console.WriteLine("Массив");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write("{0}\t", result[i, j]);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
